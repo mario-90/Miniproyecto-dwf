b03e8874f36a0da53d00796c53857f449169d4e2
mario-90 <105886653+mario-90@users.noreply.github.com>
2025-09-18 20:05:10 -0600

    base

---
+++ ProyectoF2/src/test/java/ProyectoF2/ProyectoF2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoF2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoF2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
